22f7c60abb35b10015b1b5183193b488cb89688a
Josh Broughton <josh.broughton47@gmail.com>
2021-02-11 09:35:16 -0700

    packaged the rest of the code from before into functions so it can easilly be called again with the reset button

--- script.js
+++ script.js
@@ -3,30 +3,36 @@
 //3. Add a button that will create a new grid of user-defined size
 //4. That's it!
 
-//1.
-let div = document.createElement('div');
+
+//initialize variables and set a default grid size on page loading
 let grid = document.getElementsByClassName('container')[0];
 let initLength = 25;
 
+//takes the specified length of one side of the grid and generates a square grid
+//divs which are appended under the grid container and assigned column and row 
+//starts which creates a square grid
 function drawGrid(length) {
     for (let i=1;i<length+1;i++) {
-    for (let j=1;j<length+1;j++) {
-        div = document.createElement('div');
-        div.setAttribute('class', 'box');
-        div.style.cssText = `grid-column-start:${i};grid-row-start:${j}`;
-        grid.appendChild(div);
-
-    }     
+        for (let j=1;j<length+1;j++) {
+            let div = document.createElement('div');
+            div.setAttribute('class', 'box'); //so we can edit them on mouseenter below
+            div.style.cssText = `grid-column-start:${i};grid-row-start:${j}`;
+            grid.appendChild(div);
+        }     
     }
 }
-drawGrid(initLength);    
-let boxes = document.getElementsByClassName('box')
+drawGrid(initLength);
+function sketchFunction(x) {
+    let boxes = document.getElementsByClassName('box')
+    boxes[x].classList.add("sketched");//sketched class turns boxes black
+}
+function gridListener() {
+    let boxes = document.getElementsByClassName('box')//array-like object of grid boxes
+    for (let n=0; n<boxes.length; n++) {
+        boxes[n].addEventListener("mouseenter", function(){
+            sketchFunction(n)
+        });
+    }
+}   
+gridListener();
 
-let sketchFunction = function(x) {
-    boxes[x].classList.add("sketched");
-}
-for (let n=0; n< boxes.length; n++) {
-    boxes[n].addEventListener("mouseenter", function(){
-        sketchFunction(n)
-    });
-}
